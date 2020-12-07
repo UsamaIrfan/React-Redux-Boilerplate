5bc0f48665a5ff65c7dcddc37927bd642aebd86d
Usama Irfan <58513165+UsamaIrfan@users.noreply.github.com>
2020-12-07 22:35:54 +0500

    slight tweaks on dummy data

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -2,7 +2,7 @@ const set_data = () => {
    return (dispatch) => {
       dispatch({
          type: "SET_DATA",
-         payload: { name: "Hira Mushtaq", email: "someone@love.com"}
+         payload: { name: "Hira Mushtaq", email: "someone@taime.com" }
       })
    }
 }
